d12840bf12262c95879b2f9e0da6e3cef4f18d00
X0RA <25477008+X0RA@users.noreply.github.com>
2026-02-08 04:21:44 +1100

    Make chat header clickable

--- ui/chat_window.go
+++ ui/chat_window.go
@@ -90,11 +90,10 @@ func (e *messageEntry) TypedKey(key *fyne.KeyEvent) {
 }
 
 func (c *controller) buildChatPane() fyne.CanvasObject {
-	c.chatHeader = widget.NewLabel("Select a peer to start chatting")
-	c.chatHeader.TextStyle = fyne.TextStyle{Bold: true}
-	c.chatKeyButton = newHintButtonWithIcon("", theme.InfoIcon(), "View selected peer fingerprint", c.showSelectedPeerFingerprint, c.handleHoverHint)
-	c.chatKeyButton.Disable()
-	header := container.NewBorder(nil, nil, nil, c.chatKeyButton, c.chatHeader)
+	c.chatHeader = newClickableLabel("Select a peer to start chatting", c.showSelectedPeerFingerprint)
+	c.chatHeader.SetTextStyle(fyne.TextStyle{Bold: true})
+	c.chatHeader.SetColor(colorMuted)
+	header := container.NewPadded(c.chatHeader)
 
 	emptyLabel := widget.NewLabel("No messages yet")
 	emptyLabel.Alignment = fyne.TextAlignCenter
@@ -116,7 +115,7 @@ func (c *controller) buildChatPane() fyne.CanvasObject {
 	c.chatComposer.Hide()
 
 	return container.NewBorder(
-		container.NewVBox(container.NewPadded(header), widget.NewSeparator()),
+		container.NewVBox(header, widget.NewSeparator()),
 		container.NewVBox(widget.NewSeparator(), c.chatComposer),
 		nil, nil, c.chatScroll,
 	)
@@ -126,23 +125,21 @@ func (c *controller) updateChatHeader() {
 	selectedPeerID := c.currentSelectedPeerID()
 	peerName := "Select a peer to start chatting"
 	hasPeer := false
+	statusColor := colorMuted
 	if selectedPeerID != "" {
 		if peer := c.peerByID(selectedPeerID); peer != nil {
 			hasPeer = true
 			peerName = fmt.Sprintf("%s (%s)", peer.DeviceName, peerStatusIndicator(peer.Status))
+			if strings.EqualFold(peer.Status, "online") {
+				statusColor = colorOnline
+			}
 		}
 	}
 
 	fyne.Do(func() {
 		if c.chatHeader != nil {
 			c.chatHeader.SetText(peerName)
-		}
-		if c.chatKeyButton != nil {
-			if hasPeer {
-				c.chatKeyButton.Enable()
-			} else {
-				c.chatKeyButton.Disable()
-			}
+			c.chatHeader.SetColor(statusColor)
 		}
 		if c.chatComposer != nil {
 			if hasPeer {
